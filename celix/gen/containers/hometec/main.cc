38c5dd12655a8e098828aacd48ab974d414b96eb
Sandro Garani <sandro.garani@outlook.com>
2021-04-01 19:15:54 -0300

    Commit everything

---
+++ celix/gen/containers/hometec/main.cc
@@ -0,0 +1,17 @@
+#include <celix_launcher.h>
+int main(int argc, char *argv[]) {
+    const char * config = "\
+CELIX_CONTAINER_NAME=hometec\n\
+CELIX_BUNDLES_PATH=bundles\n\
+\
+\
+\
+CELIX_AUTO_START_3=/usr/local/share/celix/bundles/shell.zip /usr/local/share/celix/bundles/remote_shell.zip /home/sandro/Workspace/hometec/bundles/hometec_service/hometec_service.zip\n\
+\
+\
+\
+";
+
+    celix_properties_t *embeddedProps = celix_properties_loadFromString(config);
+    return celixLauncher_launchAndWaitForShutdown(argc, argv, embeddedProps);
+}
